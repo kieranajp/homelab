ba39f764b7a995c3d96d9852e81ca1e8376f4f76
Kieran Patel <me@kieranajp.co.uk>
2025-12-07 12:54:07 +0000

    Add HomeAssistant

--- home-assistant.tf
+++ home-assistant.tf
@@ -1,10 +1,12 @@
-# resource "helm_release" "home_assistant" {
-#   name             = "home-assistant"
-#   repository       = "https://pajikos.github.io/home-assistant-helm-chart"
-#   chart            = "home-assistant"
-#   namespace        = "homelab"
+resource "helm_release" "home_assistant" {
+  name             = "home-assistant"
+  repository       = "https://pajikos.github.io/home-assistant-helm-chart"
+  chart            = "home-assistant"
+  namespace        = "homelab"
+  timeout          = 180
+  atomic           = true
 
-#   values = [
-#     file("${path.module}/values/home-assistant.yaml")
-#   ]
-# }
+  values = [
+    file("${path.module}/values/home-assistant.yaml")
+  ]
+}
